4d84aa3167c95c0e2c3f274e9fe57b77de2aa5f6
Romanno <sromanno@gmail.com>
2025-07-30 06:53:18 +0000

    changed index.js

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-/* Basic HelloWorld Node.JS example */
-var http = require('http');
+/* Basic Hello from POD IP example in Node.js */
+const http = require('http');
+const os = require('os');
 
-//Node function called each time our event loop receives a new HTTP request
-function onRequest(req, res){
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello ' + req.connection.remoteAddress + '!');
-    
-    /* Write the IP addresses of our connecting client to console */
-    console.log('Incoming connection from ' + req.connection.remoteAddress);
+// Funcție pentru a obține IP-ul local de pe interfața 'eth0'
+function getPodIP() {
+    const interfaces = os.networkInterfaces();
+    const eth0 = interfaces['eth0'];
+    if (!eth0) return 'unknown';
+
+    for (const iface of eth0) {
+        if (iface.family === 'IPv4' && !iface.internal) {
+            return iface.address;
+        }
+    }
+
+    return 'unknown';
 }
 
-//Listen for connections on the port provided to us by the host process
-var server = http.createServer(onRequest).listen(3000, '0.0.0.0');
+const podIP = getPodIP();
+
+function onRequest(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello ' + podIP + '!\n');
+
+    console.log('Served request from ' + req.connection.remoteAddress);
+}
+
+const port = 3000;
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Server running at http://0.0.0.0:${port}/`);
+});
